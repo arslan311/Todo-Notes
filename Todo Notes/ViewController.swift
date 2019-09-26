7d2e4755c7b5ce52d668078b8623c09e25be2be8
arslan <arslankhalid@mobizion.com>
2019-09-26 10:56:47 +0500

    Initial Commit

---
+++ Todo Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo Notes
+//
+//  Created by Arslan Khalid on 26/09/2019.
+//  Copyright © 2019 Arslan Khalid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Todo NotesTests/Todo_NotesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Todo_NotesTests.swift
+//  Todo NotesTests
+//
+//  Created by Arslan Khalid on 26/09/2019.
+//  Copyright © 2019 Arslan Khalid. All rights reserved.
+//
+
+import XCTest
+@testable import Todo_Notes
+
+class Todo_NotesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
