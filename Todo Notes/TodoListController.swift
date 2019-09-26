ab22afacf066aa281d2d3f7ae02e86ab246d3821
arslan <arslankhalid@mobizion.com>
2019-09-26 15:42:58 +0500

    add new todo dynamically completed.

--- Todo Notes/TodoListController.swift
+++ Todo Notes/TodoListController.swift
@@ -11,25 +11,23 @@ import UIKit
 class TodoListController: UITableViewController {
 
     
-    var itemsArray = ["first item","second list","third list"]
+    var itemsArray = [""]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
 //        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TodoListCell")
         tableView.separatorStyle = .none
-        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return itemsArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let row = tableView.dequeueReusableCell(withIdentifier: "TodoListCell", for: indexPath)
         row.textLabel?.text = itemsArray[indexPath.row]
-        
         return row
     }
     
@@ -81,5 +79,27 @@ class TodoListController: UITableViewController {
     }
 
 
+    //MARK - ADD A NEW TODO LIST ITEMS ON CLICK OF + BUTTON
+    
+    @IBAction func todoAddButton(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Todo Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            self.itemsArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (todoTextField) in
+            todoTextField.placeholder = "add new todo"
+            textField = todoTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+
+    }
 }
 
